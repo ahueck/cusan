ffce5b70b53f7fcd2f19c57e224448ec17b7f286
Alexander Hück <hueck.alexander@gmail.com>
2023-06-25 22:36:02 +0200

    TypeART wrapper

--- test/pass/01_test.c
+++ test/pass/01_test.c
@@ -1,5 +1,28 @@
-// RUN: %c-to-llvm %s | %apply-cucorr -S 2>&1 | %filecheck %s
+// RUN: TYPEART_WRAPPER_EMIT_IR=1 %wrapper-cc -O1 -g %s -x cuda -gencode arch=compute_50,code=sm_50 -o %s.exe
 
-// CHECK-NOT: Error
+__global__ void axpy(float a, float* x, float* y) {
+  y[threadIdx.x] = a * x[threadIdx.x];
+}
 
-int main(void) { return 0; }
+int main(int argc, char* argv[]) {
+  const int kDataLen = 4;
+
+  float a                = 2.0f;
+  float host_x[kDataLen] = {1.0f, 2.0f, 3.0f, 4.0f};
+  float host_y[kDataLen];
+
+  float* device_x;
+  float* device_y;
+  cudaMalloc((void**)&device_x, kDataLen * sizeof(float));
+  cudaMalloc((void**)&device_y, kDataLen * sizeof(float));
+
+  cudaMemcpy(device_x, host_x, kDataLen * sizeof(float), cudaMemcpyHostToDevice);
+
+  axpy<<<1, kDataLen>>>(a, device_x, device_y);
+
+  cudaDeviceSynchronize();
+  cudaMemcpy(host_y, device_y, kDataLen * sizeof(float), cudaMemcpyDeviceToHost);
+
+  cudaDeviceReset();
+  return 0;
+}
