9f9796157ed6c0b98e0506b442139f7d7d33a66d
Tim Ziegler <timziegler99@gmail.com>
2024-05-29 11:40:27 +0200

    Fixed test

--- test/pass/10_cudahostalloc.c
+++ test/pass/10_cudahostalloc.c
@@ -9,26 +9,28 @@
 // CHECK-NOT: [Error] sync
 
 // CHECK-LLVM-IR: @main(i32 noundef %0, i8** noundef %1)
-// HECK-LLVM-IR: cudaMallocHost
-// HECK-LLVM-IR: _cucorr_host_alloc
-// CHECK-LLVM-IR: cudaHostAlloc
+// CHECK-LLVM-IR: cudaMallocHost
 // CHECK-LLVM-IR: _cucorr_host_alloc
-// HECK-LLVM-IR: cudaFreeHost
-// HECK-LLVM-IR: _cucorr_host_free
+// CHECK-LLVM-IR: _ZL13cudaHostAllocIiE9cudaErrorPPT_mj
 // CHECK-LLVM-IR: cudaFreeHost
 // CHECK-LLVM-IR: _cucorr_host_free
+// CHECK-LLVM-IR: cudaFreeHost
+// CHECK-LLVM-IR: _cucorr_host_free
+// CHECK-LLVM-IR: _ZL13cudaHostAllocIiE9cudaErrorPPT_mj
+// CHECK-LLVM-IR: cudaHostAlloc
+// CHECK-LLVM-IR: _cucorr_host_alloc
 
 #include <cuda_runtime.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
   const int size            = 512;
-  //int* h_data1;
-  //cudaMallocHost(&h_data1, size*sizeof(int));
+  int* h_data1;
+  cudaMallocHost((void**)&h_data1, size*sizeof(int));
   int* h_data2;
   cudaHostAlloc(&h_data2, size*sizeof(int), cudaHostAllocDefault);
 
-  //cudaFreeHost(h_data1);
+  cudaFreeHost(h_data1);
   cudaFreeHost(h_data2);
   return 0;
 }
